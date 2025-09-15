1b592450a088499979e7e5e374a25fbd290562d6
Cillian Long <cillianlong@gmail.com>
2025-09-15 13:17:00 +0100

    Tighten up environmental variables usage in DB

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "plant",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
 });
